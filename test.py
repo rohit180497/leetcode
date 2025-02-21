c3bf463de904c1e07d51808a83a9039c331fc754
rohit180497 <rohitkosamkar97@gmail.com>
2025-02-21 18:38:39 -0500

    practice

---
+++ test.py
@@ -0,0 +1,23 @@
+class solution:
+
+    def two_sums(self, nums: list[int], target: int)-> list[int]: #hash map
+        num_dict ={}
+        for i, num in enumerate(nums):
+            comp = target - num
+            if comp in num_dict:
+                return [num_dict[comp], i]
+            num_dict[num] = i
+        return []
+    
+    def two_sums_bf(self, nums, target):#brute force
+        for i in range(0, len(nums)):
+            for j in range(i+1, len(nums)):
+                sum = nums[i] + nums[j]
+                if sum== target:
+                    return [i, j]
+                
+if __name__ == "__main__":
+    solution = solution()
+    nums = [2,4,5,7,8,9,3]
+    output = solution.two_sums_bf(nums=nums, target= 11)
+    print(output)
