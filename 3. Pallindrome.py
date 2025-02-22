4b6ab207d95a4c33ed45606f41197a721d741910
rohit180497 <rohitkosamkar97@gmail.com>
2025-02-21 20:02:45 -0500

    done for today

---
+++ 3. Pallindrome.py
@@ -0,0 +1,42 @@
+"""
+Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+
+"""
+
+class solution:
+
+    def pallindrome(self, num: int) -> int: 
+        if num<0:
+            return False #negative numbers are not pallindrome
+        rev_num = str(num)[::-1]
+        if num == int(rev_num):
+            return True
+        else:
+            False
+
+                
+if __name__ == "__main__":
+    solution = solution()
+    number = 12121
+    output = solution.pallindrome(number)
+    print(output)
