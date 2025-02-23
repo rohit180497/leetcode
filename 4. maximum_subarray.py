da0a335ff6011080c4e45cca3a29a7937d83899f
rohit180497 <rohitkosamkar97@gmail.com>
2025-02-22 22:03:09 -0500

    max_subarray

---
+++ 4. maximum_subarray.py
@@ -0,0 +1,50 @@
+"""
+Given an integer array nums, find the 
+subarray
+ with the largest sum, and return its sum.
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+ 
+
+Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+"""
+
+
+class solution:
+
+    def maximum_sub(self, nums: int) -> int:    #brute force approach
+        max_sum =  float('-inf')
+        for i in range(len(nums)):
+            current_sum = 0
+            for j in range(i, len(nums)):
+                current_sum += nums[j]
+                max_sum = max(max_sum, current_sum)
+        
+        return max_sum
+
+                
+if __name__ == "__main__":
+    solution = solution()
+    number = [-2,1,-3,4,-1,2,1,-5,4]
+    output = solution.maximum_sub(number)
+    print(output)
